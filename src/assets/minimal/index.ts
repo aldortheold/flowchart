35489ceb29646bfcea78b2f09c2a86b1678f6c57
Andrew <aldortheold@gmail.com>
2026-07-22 22:49:48 +1000

    added flower buttons & bg updates

---
+++ src/assets/minimal/index.ts
@@ -0,0 +1,3 @@
+export { default as rose } from "./rose.svg";
+export { default as tulip } from "./tulip.svg";
+export { default as daisy } from "./daisy.svg";
--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,15 +1,74 @@
+import { useState } from "react"
 import { motion } from "framer-motion"
+import * as botanical from "../assets/botanical"
+import * as minimal from "../assets/minimal"
+import "./Editor.css"
 
 function Editor() {
+
+    const [bg, setBg] = useState("#f3f3f3");
+
     return (
         <motion.main
-            className='editor'
+            className="editor"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.5 }}
         >
-            <h1>Editor</h1>
+            <div className="canvas">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1600 900"
+                    preserveAspectRatio="xMidYMid meet"
+                    width="100%"
+                    height="100%"
+                >
+                    <rect
+                        x="0"
+                        y="0"
+                        width="1600"
+                        height="900"
+                        fill={bg}
+                    />
+                </svg>
+            </div>
+            <div className="sidebar">
+                <section className="canvas-editor">
+                    <h2>Canvas</h2>
+                    <hr />
+                    <div className="field">
+                        <label htmlFor="background-color">Background color</label>
+                        <input
+                            type="color"
+                            id="background-color"
+                            value={bg}
+                            onChange={(e) => setBg(e.target.value)}
+                        />
+                    </div>
+                    <hr />
+                </section>
+                <section className="flowers-editor">
+                    <h2>Flowers</h2>
+                    <hr />
+                    <h3>Botanical</h3>
+                    <div className="flower-menu">
+                    {Object.keys(botanical).map((key) =>
+                        <button key={key}>
+                            <img src={botanical[key as keyof typeof botanical]} />
+                        </button>
+                    )}
+                    </div>
+                    <h3>Minimal</h3>
+                    <div className="flower-menu">
+                    {Object.keys(minimal).map((key) =>
+                        <button key={key}>
+                            <img src={minimal[key as keyof typeof minimal]} />
+                        </button>
+                    )}
+                    </div>
+                </section>
+            </div>
         </motion.main>
     )
 }
